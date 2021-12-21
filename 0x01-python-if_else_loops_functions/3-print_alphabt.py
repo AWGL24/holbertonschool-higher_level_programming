e7fff772878d368fcd0c6185fb4c3a363db64b27
Drew-24 <andrewwil1@hotmail.com>
2021-12-21 17:08:45 +0000

    When i was having that alphabet soup, i never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for c in range(97, 122):
-    if c == 101 or c == 113:
-        continue
-    print("{}".format(chr(c)), end="")
+for c in range(97, 123):
+    if c not in [101, 113]:
+        print("{}".format(chr(c)), end="")
