49b3e5e10c2e52ad0eccb391c21e8b15592e38e8
Drew-24 <andrewwil1@hotmail.com>
2022-01-19 03:27:57 +0000

    Size Validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
+""" Module with the class Square """
+
+
 class Square:
+    """ Square Defined """
     __size = None
 
     def __init__(self, size=0):
+        """ Docstring of __init___ method
+        Args:
+            size (int): size from main to be displayed
+        """
         if (type(size) != int):
             raise(TypeError("size must be an integer"))
         if (size < 0):
             raise(ValueError("size must be >= 0"))
         self.__size = size
+        """ int: Docstring *after* attribute, with type specified """
