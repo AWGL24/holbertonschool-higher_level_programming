d930c1a44d3d9bc426313c8b36d9aababe1054fc
Drew-24 <andrewwil1@hotmail.com>
2022-01-31 15:06:37 -0400

    inherits_from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" inherits_from """
+
+
+def inherits_from(obj, a_class):
+    if issubclass(type(obj), a_class) and not type(obj) is a_class:
+        return True
+    else:
+        return False
