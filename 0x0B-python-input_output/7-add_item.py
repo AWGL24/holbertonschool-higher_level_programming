2764ef7c21b6ac11de64a8db65299c8617202122
Drew-24 <andrewwil1@hotmail.com>
2022-02-02 14:39:25 -0400

    Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module holds code """
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+filename = "add_item.json"
+open(filename, 'a')
+try:
+    MyList = load_from_json_file(filename)
+except ValueError:
+    MyList = []
+save_to_json_file(MyList + sys.argv[1:], filename)
