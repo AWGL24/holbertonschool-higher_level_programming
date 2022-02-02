3dc0c4c2d92deead9ef0551baecd602c4dc22404
Drew-24 <andrewwil1@hotmail.com>
2022-02-02 09:13:46 -0400

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,5 @@ def read_file(filename=""):
         param:
             filename="": place holder for the file's name
     """
-    with open(filename, encoding="utf-8") as MyFile:
-        print(MyFile.read())
+    with open(filename) as MyFile:
+        print(MyFile.read(), end="")
