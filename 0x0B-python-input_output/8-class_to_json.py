f1b884fe3add8d2253b9746c503f4fca79e868e2
Drew-24 <andrewwil1@hotmail.com>
2022-02-02 20:19:06 -0400

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" Module holds functon that returns the dict description for JSON sterilization of an obj """
+""" Module holds functon that returns the dict description for JSON sterilization of an obj
+"""
 
 
 def class_to_json(obj):
-    """ returns dict description for JSON """
+    """ returns dict description for JSON
+    """
     return obj.__dict__
