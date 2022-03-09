b5a63392acc072978a75cc02fdf5f9e725374daa
Drew-24 <andrewwil1@hotmail.com>
2022-03-09 16:38:55 -0400

    Fixed code

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,3 @@
 -- Lists all records of the table second_table
 -- database
-SELECT score, name FROM second_tabe WHERE name IS NOT NULL ORDER BY score DESC
-
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC
