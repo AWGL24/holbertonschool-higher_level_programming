88416afa1499a141f5e9880b3425c45b4602cdf3
Drew-24 <andrewwil1@hotmail.com>
2022-03-10 10:57:19 -0400

    MYNGENRES

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- import the database dump from htbn_0d_tvshows
-SELECT tv_genres.name as 'name'
+SELECT tv_genres.name AS 'name'
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id =  tv_shows.id
 WHERE tv_shows.title = 'Dexter'
