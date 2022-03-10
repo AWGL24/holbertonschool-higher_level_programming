41a57472648c8aaf505d147012f478066939ecd1
Drew-24 <andrewwil1@hotmail.com>
2022-03-09 21:05:05 -0400

    Fixed code

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
--- Lists all cities contained in the database hbtn_0d_usa
+-- Lists all cities contained in the database
 SELECT cities.id, cities.name, states.name
 FROM cities
 INNER JOIN states ON cities.state_id=state.id
