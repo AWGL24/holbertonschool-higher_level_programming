3bab2a7d7cdb6fb9df32226fb98cde580825bab0
Drew-24 <andrewwil1@hotmail.com>
2022-03-10 10:59:38 -0400

    My Genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,5 +2,6 @@
 SELECT tv_genres.name AS 'name'
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id =  tv_shows.id
+INNER JOIN tv_shows ON  tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY name;
