af9d44aa01a52fb5ed93dbb1aa3cfbb5c326b2a2
Drew-24 <andrewwil1@hotmail.com>
2022-04-04 19:27:28 -0400

    Fixed code

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,12 +2,10 @@
 // Script that prints x times "C is fun"
 
 const count = process.argv[2];
-let i = 0;
-
 if (isNaN(count)) {
 	console.log('Missing number of occurrences');
 } else {
-	for (; i < x; i++) {
+	for (let i = 0; i < x; i++) {
 		console.log('C is fun');
 	}
 }
