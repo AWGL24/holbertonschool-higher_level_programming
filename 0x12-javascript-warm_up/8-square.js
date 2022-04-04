1fbf2f1d0fb7983acb06098b0e87620552b932b5
Drew-24 <andrewwil1@hotmail.com>
2022-04-04 19:36:05 -0400

    Fixed code

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,9 +5,9 @@ const count = process.argv[2];
 let i = 0;
 
 if (parseInt(count)) {
-	for (; i < count; i++) {
-		console.log('X'.repeat(count));
-	}
+  for (; i < count; i++) {
+    console.log('X'.repeat(count));
+  }
 } else {
-	console.log('Missing size');
+  console.log('Missing size');
 }
