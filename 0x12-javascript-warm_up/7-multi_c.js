6fbe1802033b47b98cffa69ea98a95cddf3dc12e
Drew-24 <andrewwil1@hotmail.com>
2022-04-04 19:11:38 -0400

    Fixed code

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ let count = process.argv[2];
 if (isNaN(count)) {
 	console.log('Missing number of occurrences');
 } else {
-	for (count = 0; count > 0; count--) {
+	for (; count > 0; count--) {
 		console.log('C is fun');
 	}
 }
