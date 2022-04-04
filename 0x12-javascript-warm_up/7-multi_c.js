cd1f06b8c335e2cf8be81458ecb180565c43f2a1
Drew-24 <andrewwil1@hotmail.com>
2022-04-04 19:30:28 -0400

    Fixed code for the last time

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-// Script that prints x times "C is fun"
+// Print C is fun x times
 
-const count = process.argv[2];
-if (isNaN(count)) {
+const x = process.argv[2];
+if (isNaN(x)) {
 	console.log('Missing number of occurrences');
 } else {
-	for (let i = 0; i < count; i++) {
+	for (let i = 0; i < x; i++) {
 		console.log('C is fun');
 	}
 }
