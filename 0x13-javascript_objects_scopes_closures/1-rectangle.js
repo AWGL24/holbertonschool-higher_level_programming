b41f75eff88b77486250bb03e95882218b8babe1
Drew-24 <andrewwil1@hotmail.com>
2022-04-05 12:08:38 -0400

    fixed semistandard

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,8 +2,8 @@
 // Write a class rectangle that defines a rectangle
 
 module.exports = class Rectangle {
-	constructor(w, h) {
-		this.width = w;
-		this.height = h;
-	}
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 };
