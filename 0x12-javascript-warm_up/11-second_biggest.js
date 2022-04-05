c902d446995977ce8c32412df68df61274dfa86f
Drew-24 <andrewwil1@hotmail.com>
2022-04-04 22:36:11 -0400

    Fixed code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 // searches the second biggest integer in the list of arguments.
 
-// creates new array of numbers using maps
-// only selects the real arguements
 const args = process.argv.slice(2).map(Number);
 let Bigsecond = 0;
 
