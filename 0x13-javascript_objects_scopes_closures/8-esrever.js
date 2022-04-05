5ee54902386ad8c0ed5512578d1d703b11e65a37
Drew-24 <andrewwil1@hotmail.com>
2022-04-05 19:43:33 -0400

    Fixed code

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,11 +2,11 @@
 // Write a function that returns the reversed version of a list
 
 exports.esrever = function (list) {
-	const reverlist = [];
-	let i = list.lenght - 1;
-	while (i >= 0) {
-		reverlist.push(list[i]);
-		i--;
-	}
-	return (reverlist);
+  const reverlist = [];
+  let i = list.lenght - 1;
+  while (i >= 0) {
+    reverlist.push(list[i]);
+    i--;
+  }
+  return (reverlist);
 };
