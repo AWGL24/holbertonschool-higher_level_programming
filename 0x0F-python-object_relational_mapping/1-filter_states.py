af9e44455b82f6d1f674d5dc3b480096168a9043
Drew-24 <andrewwil1@hotmail.com>
2022-04-09 15:03:16 -0400

    Fixed code

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-''' Write a script that lists all states with a name starting with N from a database '''
+''' Write a script that lists all states with a name
+starting with N from a database '''
 
 import MySQLdb
 from sys import argv
