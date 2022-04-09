810729295bed31a971b22ff7d630df01407fab73
Drew-24 <andrewwil1@hotmail.com>
2022-04-09 18:25:58 -0400

    Update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -4,14 +4,13 @@ of a State object from the database '''
 
 import MySQLdb
 from sys import argv
-
 from requests import Session
 from model_state import State, Base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, text
 
 if __name__ == "__main__":
-    engine = create_engine('mysql=mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
