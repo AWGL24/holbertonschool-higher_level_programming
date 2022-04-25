764161da7945befc3a39af76c56cbf606542fab5
Drew-24 <andrewwil1@hotmail.com>
2022-04-25 10:40:23 -0400

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,9 +5,9 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    res = requests.get(argv[1])
-    status = res.status_code
+    response = requests.get(argv[1])
+    status = response.status_code
     if status >= 400:
-        print("Error cose: {}".format(status))
+        print("Error code: {}".format(status))
     else:
-        print(res.content.decode('utf8'))
+        print(response.content.decode('utf8'))
