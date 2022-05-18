d5cf9ce73bd8a8c64a19b8db69268f86b6a7c442
Drew-24 <andrewwil1@hotmail.com>
2022-05-18 10:43:41 -0400

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Script that displays the status code of a GET request
+
+const https = require('https');
+const URL = process.argv[2];
+https.get(URL, function (res) {
+  console.log('code:', res.statusCode);
+});
