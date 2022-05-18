41c91e7d54488bfeabcc53ea32af39049aa7d79e
Drew-24 <andrewwil1@hotmail.com>
2022-05-18 11:13:18 -0400

    Fixed code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,11 +2,7 @@
 // Script that displays the status code of a GET request
 
 const https = require('https');
-const url = process.argv[2];
-https.get(url, function (err, response) {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log('code:' + response.statusCode);
-	}
+const URL = process.argv[2];
+https.get(URL, function (res) {
+	console.log('code:', res.statusCode);
 });
