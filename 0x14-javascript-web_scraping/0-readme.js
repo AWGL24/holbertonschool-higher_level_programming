ea61937cab9f71afc0c46b827dc047ac3ffc10d9
Drew-24 <andrewwil1@hotmail.com>
2022-05-18 10:00:37 -0400

    Fixed code

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // Write a script that reads and prints the content of a file.
 
-let fs = require('fs');
-let file = process.argv[2];
-let data = fs.readFileSync(file, 'utf8');
+const fs = require('fs');
+const file = process.argv[2];
+const data = fs.readFileSync(file, 'utf8');
 fs.readFile(file, function (err, data) {
-	if (err) return console.error(err);
-	console.log(data.toString());
+  if (err) return console.error(err);
+  console.log(data.toString());
 });
